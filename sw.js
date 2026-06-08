ee12f9660c7ff4bc06599727f7a81dabee06bdc2
SangdangPol <goldburg18@gmail.com>
2026-06-08 12:14:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,41 @@
-// 서비스 워커 (Service Worker)
+const CACHE_NAME = 'sangdang-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './voice/index.html',
+  './manager/index.html',
+  './check/index.html',
+  './defender/index.html'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('치안센터 서비스 워커 활성화');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
