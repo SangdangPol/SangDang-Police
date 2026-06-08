ac732e96c38cb5543081249f9595b78bf1bd90bc
SangdangPol <goldburg18@gmail.com>
2026-06-08 13:51:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,3 +6,5 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     console.log('치안센터 서비스 워커 활성화');
 });
+self.addEventListener('fetch', (event) => {
+});
