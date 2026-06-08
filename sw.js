632c79e847aca02acf60526ed518b3596d571a60
SangdangPol <goldburg18@gmail.com>
2026-06-08 13:56:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,4 +7,10 @@ self.addEventListener('activate', (event) => {
     console.log('치안센터 서비스 워커 활성화');
 });
 self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            console.log('오프라인 상태 접근 감지');
+            // 향후 오프라인 안내 페이지 연결 가능
+        })
+    );
 });
